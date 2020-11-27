25bea315a0b8e02d39730ffac12cb6b78b0aa175
Abhishek <abhishekbanthia@me.com>
2020-11-26 18:34:07 -0600

    sup!

--- src/pages/404.js
+++ src/pages/404.js
@@ -10,8 +10,8 @@ const NotFoundPage = ({ data, location }) => {
   return (
     <Layout location={location} title={siteTitle}>
       <SEO title="404: Not Found" />
-      <h1>404: Not Found</h1>
-      <p>You just hit a page that doesn&#39;t exist... the sadness.</p>
+      <h1>Page Not Found :(</h1>
+      <p>Whatever you're looking for isn't here. If this feels like a bug, don't hesitate to shoot an email <a href="mailto:abhishekbanthia@me.com">here</a>.</p>
     </Layout>
   )
 }
--- src/pages/clockersupport.js
+++ src/pages/clockersupport.js
@@ -3,6 +3,30 @@ import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import styled from 'styled-components'
+
+const TextContainer = styled.textarea`
+ width: 400px;
+ height: 75px; 
+ max-height: 150px;
+ max-width: 400px;
+`
+const RoundedDiv = styled.div` 
+ background-color: white;
+ border: none;
+ color: black;
+ padding-right: 40px;
+ padding-bottom: 20px;
+ padding-left: 15px;
+ text-align: left;
+ text-decoration: none;
+ display: inline-block;
+ border-radius: 8px;
+`
+const FieldName = styled.p` 
+  margin-bottom: 1px;
+  margin-top: 15px;
+`
 
 const ClockerSupport = ({ data, location }) => {
 	const siteTitle = data.site.siteMetadata?.title || `Title`
@@ -10,6 +34,29 @@ const ClockerSupport = ({ data, location }) => {
 		<Layout location={location} title={siteTitle}>
         	<SEO title="Clocker Support" />
         	<h1> Clocker Support </h1>
+          <hr/>
+          <p> For faster responses, please use the in-app feedback window 
+            by navigating to <b>Preferences</b> -> <b>About</b> tab and clicking on the <b>Report Issue</b> button.
+          </p>
+          <RoundedDiv>
+          <form 
+            name="clocker-feedback" 
+            action="clockerformsubmission">
+            <h3> your feedback/suggestion </h3>
+            <FieldName><b>Name*</b></FieldName>
+            <input name="name" type="text" />
+            <FieldName><b>Email*</b></FieldName>
+            <input name="email" type="email" />
+            <FieldName><b>Type</b></FieldName>
+            <select name="type">
+              <option value="Suggestion">Suggestion</option>
+              <option value="Bug">Bug</option>
+            </select>
+            <FieldName><b>Comment*</b></FieldName>
+            <TextContainer name="feedback" /><br/><br/>
+            <button><b>Submit</b></button>
+          </form>
+          </RoundedDiv>
 		</Layout>
 	)
 }
