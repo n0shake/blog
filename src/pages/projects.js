ef6c89be328676456716bffaf14c4f90d57152f6
Abhishek Banthia <8280282+n0shake@users.noreply.github.com>
2024-04-19 16:08:46 -0400

    Update projects.js

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -14,7 +14,7 @@ const Projects = ({ data, location }) => {
             	<hr/><br/>
                 <ul>
             	<li> 
-            		<a href="https://github.com/n0shake/Clocker">Clocker</a> is a menu bar utility to keep track of your friends in different time zones. 415K+ downloads. 
+            		<a href="https://github.com/n0shake/Clocker">Clocker</a> is a menu bar utility to keep track of your friends in different time zones. <b>App Store's Editor Choice. 415K+ downloads.</b>
             	</li>
             	<li> 
             		<a href="https://github.com/n0shake/Public-APIs">Public-Api(s)</a> is a curated list of common and obscure APIs from around the web. 
