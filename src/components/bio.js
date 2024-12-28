5b468845746b535bffddbe22905b4771308553ec
Abhishek Banthia <8280282+n0shake@users.noreply.github.com>
2024-12-28 18:04:32 -0500

    Update bio.js

--- src/components/bio.js
+++ src/components/bio.js
@@ -25,7 +25,7 @@ const Bio = () => {
       {author?.name && (
         <p>
           hey! i'm an engineer working out of <a href={`https://en.wikipedia.org/wiki/Richmond,_Virginia`}>Richmond, VA</a>.
-          i'm available to chat via <a href={`mailto:abhishekbanthia@me.com`}>email</a> or <a href={`https://twitter.com/${social?.twitter || ``}`}>x / twitter</a> or check out my projects <a href={`https://github.com/n0shake`}>here</a>.
+          i'm available to chat via <a href={`mailto:abhishekbanthia@me.com`}>email</a> or <a href={`https://twitter.com/${social?.twitter || ``}`}>x / twitter</a> or check out my side-projects <a href={`https://github.com/n0shake`}>here</a>.
         </p>
       )}
     </div>
